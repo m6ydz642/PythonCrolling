96f4ac08623d6b57e956e2d6c785c164ccfd2234
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2021-01-29 22:33:35 +0900

    db 모듈 안되서 insert구문으로만듦 ㅡ.ㅡ

--- Economy.py
+++ Economy.py
@@ -6,12 +6,10 @@ import jaydebeapi # h2 DB api
 
 context = ssl._create_unverified_context()
 
-------------------------------------------------
-
-
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup  #bs모듈의 BeautifulSoup함수를 사용 하기 위해 bs4모듈을 읽어 들임
+
 
 
 target = urlopen("https://www.hankyung.com/economy", context=context).read().decode('utf-8')
@@ -19,22 +17,28 @@ target = urlopen("https://www.hankyung.com/economy", context=context).read().dec
 soup = BeautifulSoup(target, "html.parser")
 # print("soup 내용 ", soup)
 for list in soup.select("div .article"):
-    # <list>...</list>태그내부의 thstrm_nm, sj_nm, account_nm, thstrm_amount 태그를 찾아 출력합니다.
     title = list.select_one("h3 > a").text
-    subject = list.select_one("p").text
-    print("제목 : ", title)
-    print("본문 : " , subject)
+    content = list.select_one("p").text
 
+    insertDB = "insert into board (userid, subject, content) values ('기자양반',"
+    insertTitle = " ' " + title + " ', "
+    insertcontent = " ' " + content + " ' ); "
+    output = insertDB + insertTitle + insertcontent
+    print (output)
 
+    # print("제목 : ", title)
+    # print("본문 : " , content)
+    text = open('data.txt', 'a')  #
+    text.write(output + "\n")
+text.close()
+# conn = jaydebeapi.connect('org.h2.Driver', ['jdbc:h2:~/test', 'sa', ''],
+#                           '/Users/m6ydz642/git/SpringNoMaven/WebContent/WEB-INF/lib/h2-1.4.200.jar')
+# curs = conn.cursor()
+# curs.execute('create table PERSON ("PERSON_ID" INTEGER not null, "NAME" VARCHAR not null, '
+#              'primary key ("PERSON_ID"))')
+# curs.execute("insert into PERSON values (1, 'John')")
+# curs.execute("select * from PERSON")
+# data = curs.fetchall()
+# print(data)
 
-
-conn = jaydebeapi.connect('org.h2.Driver', ['jdbc:h2:~/test', 'sa', ''],
-                          '/Users/m6ydz642/git/SpringNoMaven/WebContent/WEB-INF/lib/h2-1.4.200.jar')
-curs = conn.cursor()
-curs.execute('create table PERSON ("PERSON_ID" INTEGER not null, "NAME" VARCHAR not null, '
-             'primary key ("PERSON_ID"))')
-curs.execute("insert into PERSON values (1, 'John')")
-curs.execute("select * from PERSON")
-data = curs.fetchall()
-print(data)
 
